146ae5d6c226247d6b780944e74c124bd8417962
Tom <hernandeztomas584@gmail.com>
2024-11-07 23:14:56 -0300

    add: comentarios

--- guias/soluciones/8.pl
+++ guias/soluciones/8.pl
@@ -7,6 +7,6 @@ padre(luis, manuel).
 padre(luis, ramiro).
 abuelo(X,Y) :- padre(X, Z), padre(Z, Y).
 hijo(X,Y) :- padre(Y, X).
-hermano(X,Y) :- hijo(X, Z), hijo(Y, Z), X \= Y. /* Tengo que garantizar que uno NO es hermano de si mismo. */
+hermano(X,Y) :- hijo(X, Z), hijo(Y, Z), X \= Y. /* si X \= Y lo pongo al pricnipio no vale porque si Y no estaria instanciada - enviada por parametro fallaria porque ej: hermano(juan, Y) no unifica juan con Y */ 
 descendiente(X,Y) :- hijo(X,Y).
 descendiente(X, Y) :- hijo(X, Z), descendiente(Z, Y).    
