c6642ae14487faf9c0f2106eeaf99b5872abdb9d
Tom <hernandeztomas584@gmail.com>
2024-11-24 20:43:22 -0300

    add: funciones de prolog

--- parciales/prolog.pl
+++ parciales/prolog.pl
@@ -2,7 +2,7 @@ estudiante(tomas).
 estudiante(angel).
 notas([(tomas, tda, 3), (tomas, plp, 10), (angel, plp, 3), (angel, plp, 10)]).
 /* aca como tieneMateriaAprobada E y M estan instanciados member((tomas, plp, Nota), [()...]) unifica con [(tomas, plp, 10)] y luego chequea que esa unificacion de tomas, plp tenga nota >=4 */
-tieneMateriaAprobada(E, M) :- estudiante(E), notas(XS), member((E, M, Nota), XS), Nota >= 4.
+/* tieneMateriaAprobada(E, M) :- estudiante(E), notas(XS), member((E, M, Nota), XS), Nota >= 4.
 
 eliminarAplazos([], []). 
 eliminarAplazos([(E, M, N) | XS], [(E, M, N) | Res]) :- N >= 4, eliminarAplazos(XS, Res).
@@ -13,4 +13,15 @@ notasDeAlumno(_, [], []).
 notasDeAlumno(E, [(E, _, N) | XS], [N | Res]) :- notasDeAlumno(E, XS, Res).
 notasDeAlumno(E, [(E2, _, N) | XS], Res) :- E\=E2, notasDeAlumno(E, XS, Res).
 
-promedio(E, P) :- estudiante(E), notas(XS), eliminarAplazos(XS, Res), notasDeAlumno(E, Res, Res2), sum_list(Res2, Res3), length(Res2, Res4), P is Res3 / Res4.
+promedio(E, P) :- estudiante(E), notas(XS), eliminarAplazos(XS, Res), notasDeAlumno(E, Res, Res2), sum_list(Res2, Res3), length(Res2, Res4), P is Res3 / Res4. */
+
+tieneMateriaAprobada(E, M) :- estudiante(E), notas(L), member((E,M,N), L), N>=4.
+
+eliminarAplazos([], []).
+eliminarAplazos([(E,M,N) | T], [(E, M, N) | L]) :- N>=4, eliminarAplazos(T, L).
+eliminarAplazos([(E, M, N) | T], [(E, M, N) | L]) :- N<4, not(tieneMateriaAprobada(E, M)),  eliminarAplazos(T, L).
+eliminarAplazos([(E, M, N) | T], L) :- N<4, tieneMateriaAprobada(E, M),  eliminarAplazos(T, L).
+
+notasEstudiante(E, [], []).
+notasEstudiante(E, [(E, M, N) | T], [(E, M, N) | Res]) :- notasEstudiante(E, T, Res).
+notasEstudiante(E, [(E2, M, N) | T], Res) :- E \= E2, notasEstudiante(E, T, Res).
