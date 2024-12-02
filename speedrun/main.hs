81ba0a330e857a8d4412a19c647ff84868b9c59b
Tom <hernandeztomas584@gmail.com>
2024-12-02 17:52:58 -0300

    add: haskell

--- speedrun/main.hs
+++ speedrun/main.hs
@@ -31,4 +31,26 @@ puedeCompletarLecturas = recBuffer True (\_ _ _ rec -> rec) (\b bs rec -> case c
         Just _  -> rec)
 
 
+data Componente = Contenedor | Motor | Escudo | Cañon deriving Eq
+data NaveEspacial = Módulo Componente NaveEspacial NaveEspacial | Base Componente deriving Eq 
 
+recNave :: (Componente -> NaveEspacial -> NaveEspacial -> b -> b -> b) -> (Componente -> b) -> NaveEspacial -> b 
+recNave fNave fBase nave = case nave of  
+    Módulo c n1 n2 -> fNave c n1 n2 (rec n1) (rec n2)
+    Base c -> fBase c 
+    where rec = recNave fNave fBase
+
+foldNave :: (Componente -> b -> b -> b) -> (Componente -> b) -> NaveEspacial -> b 
+foldNave fModulo fBase = recNave(\c _ _ n1 n2 -> fModulo c n1 n2) fBase
+
+espejo :: NaveEspacial -> NaveEspacial
+espejo = foldNave(\c n1 n2 -> Módulo c n2 n1) Base
+
+{- Dada dos naves. Indica si la primera esta contenido dentro de la segunda.
+    Debe suceder que la NaveEspacial (primera) este en la segunda nave como subarboles sin otros componentes mas abajo, y ademas, no tienen que ser iguales a la segunda nave.
+-}
+esSubnavePropia :: NaveEspacial -> NaveEspacial -> Bool 
+esSubnavePropia n1 = recNave(\_ sn1 sn2 r1 r2 -> sn1 == n1 || sn2 == n1 || r1 || r2) (const False) 
+
+truncar :: NaveEspacial -> Integer -> NaveEspacial
+truncar = recNave(\c sn1 sn2 r1 r2 -> (\i -> (if i == 0 then (Base c) else Módulo c (r1 (i-1)) (r2 (i-1))))) (\c -> (\i -> (Base c)))
