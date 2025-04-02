3d46dc2e789909579d8433c4f0bb7d8fd827cab6
Tom <hernandeztomas584@gmail.com>
2025-04-02 18:25:43 -0300

    add: quemador de craneo

--- guias/soluciones/12.hs
+++ guias/soluciones/12.hs
@@ -1,5 +1,5 @@
 data AB a = Nil | Bin (AB a) a (AB a)
-
+{-arbol = (Bin (Bin Nil 10 (Bin (Bin Nil 150 Nil)  100 Nil))  5 (Bin (Bin Nil 99 Nil) 10 Nil))-}
 foldAB :: b -> (b -> a -> b -> b) -> AB a -> b 
 foldAB z fBin Nil = z
 foldAB z fBin (Bin i r d) = fBin (rec i) r (rec d)
@@ -20,3 +20,12 @@ altura = foldAB 0 (\ri r rd -> 1 + (max ri rd))
 
 cantNodos :: AB a -> Int 
 cantNodos = foldAB 0 (\ri r rd -> 1 + ri + rd)
+
+{- mejorSegunAB :: (a -> a -> Bool) -> AB a -> a 
+mejorSegunAB f (Bin i r d) = foldAB(\ri r rd -> if f ri rd then (if f r ri then r else ri) else (if f r rd then r else rd)) (Bin i r d) -}
+
+ramas :: AB a -> [[a]]
+ramas = foldAB [] (\ri r rd -> if null ri && null rd then [[r]] else map (r :) ri ++ map (r :) rd)
+
+cantHojas :: AB a -> Int 
+cantHojas = foldAB 0 (\ri r rd -> (if  (== 0) ri && (== 0) rd then 1 else 0) + ri + rd)
