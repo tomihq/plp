88ad5bdd7ce02d7bb2de087b8ff33cca05ce9db7
Tom <hernandeztomas584@gmail.com>
2025-04-02 17:25:26 -0300

    add: sumasParciales

--- guias/soluciones/3.hs
+++ guias/soluciones/3.hs
@@ -19,7 +19,7 @@ map f = foldr(\x rec -> (:) (f x) rec) []
     if (>1(if(> 10 3) then 10 else 3)) else (if(> 10 3) then 10 else 3) -> Acá podríamos usar programación dinámica si estuvieramos en I.P.
     Todas las ramas van arrojando si o si un numero. La ejecución es lazy, pero el operador > necesita si o si dos argumentos atómicos de ambos lados, entonces resuelve hasta el caso base.
 
-    Plega hacia la derecha según la decisión que necesite ir tomando. Acá en este caso se duplican "casos" porque el > te obliga si o si ir hasta la profundidad, obtener un resultado. El resultado después lo arma al final.
+    Plega tanto hacia la derecha según la decisión que necesite ir tomando. Acá en este caso se duplican "casos" porque el > te obliga si o si ir hasta la profundidad, obtener un resultado. El resultado después lo arma al final. Toda guarda que requiera evaluar una entrada recursiva, va a tener que ir hasta el final del plegado (caso base). 
 
     Por ej.: recr(\x xs rec -> if(x > e) then e:x:xs else x:rec) e = 2 [1, 2, 3]
     > if(1 > 2)? Como acá ya tiene los dos argumentos calculados decide al toque. En este caso se va al else 1:(if(2 > 2)?) como la guarda es falsa, va de vuelta al else. 1:(2:(if(3 > 2))) como la guarda es verdadera, ahora sí va al then. 1:(2:(2:[3])) = [1, 2, 2, 3]
@@ -27,6 +27,10 @@ map f = foldr(\x rec -> (:) (f x) rec) []
 
 mejorSegun :: (a -> a -> Bool) -> [a] -> a 
 mejorSegun f = foldr1(\x rec -> if(f x rec) then x else rec)
+
+{- Foldl: Empieza resolviendo desde el primer elem, entonces el primer elem lo pone a lo ultimo si hablamos de agregar a lista.-}
+sumasParciales :: Num a => [a] -> [a]
+sumasParciales = reverse . foldl (\ac x -> if(length ac > 0) then (head ac + x) : ac else x:ac) []
 
 sumaAlt :: Num a => [a] -> a 
 sumaAlt = foldr (-) 0 
