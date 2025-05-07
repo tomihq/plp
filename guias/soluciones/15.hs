3e40a1f59c1a573bba6d930dfe394bf69ec1b5c7
Tom <hernandeztomas584@gmail.com>
2025-05-06 23:08:55 -0300

    add: codigo

--- guias/soluciones/15.hs
+++ guias/soluciones/15.hs
@@ -4,11 +4,11 @@ foldRose :: (a -> [b] -> b) -> Rosetree a -> b
 foldRose fRose (Rose a hijos) = fRose a (map (foldRose fRose) hijos)
 
 hojas :: Rosetree a -> [a]
-hojas = foldRose(\r rec -> if (length(rec) == 0) then [r] else concat rec)
+hojas = foldRose(\r rec -> if ((==0) . length) rec then [r] else concat rec)
 
 {-Pienso caso base primero: [0]-}
 distancias:: (Num a) => Rosetree a -> [a]
 distancias = foldRose(\_ rec -> 0:concatMap(map(+1)) rec)
 
 altura :: (Ord a, Num a) => Rosetree a -> a
-altura r = maximum(map(+1)(distancias r))
+altura r = maximum $ map(+1)(distancias r)
