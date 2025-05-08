718ec25b554bcf8b67cecf9af062f7f137256d56
Tom <hernandeztomas584@gmail.com>
2025-05-08 19:20:40 -0300

    add: haskell

---
+++ guias/soluciones/folds/main.hs
@@ -0,0 +1,30 @@
+data Operador = Sumar Int | DividirPor Int | Secuencia [Operador] deriving Show
+
+foldOpe :: (Int -> b) -> (Int -> b) -> ([b] -> b) -> Operador -> b 
+foldOpe fSuma fDiv fSec op = case op of 
+    Sumar n -> fSuma n 
+    DividirPor n -> fDiv n
+    Secuencia ops -> fSec $ map(foldOpe fSuma fDiv fSec) ops
+
+falla :: Operador -> Bool
+falla = foldOpe(const False)(==0)(elem True)
+
+aplanarSecuencias :: Operador -> [Operador] 
+aplanarSecuencias (Secuencia ops) = ops
+aplanarSecuencias ops = [ops]
+
+aplanar :: Operador -> Operador
+aplanar = foldOpe (\n -> Sumar n)(\n -> DividirPor n) (\r -> Secuencia(concatMap aplanarSecuencias r))
+
+componerTodas :: [a -> a] -> (a -> a)
+componerTodas = foldl (.) id
+
+aplicar :: Operador -> Int -> Maybe Int
+aplicar op n 
+    | falla op = Nothing
+    | otherwise = Just (componerTodas fun n)
+            where fun = reverse(foldOpe
+                        (\x -> [(+ x)])
+                        (\x -> [\n -> n `div` x])
+                        concat
+                        (aplanar op))
