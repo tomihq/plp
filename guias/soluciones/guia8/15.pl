9cb3f0834f88c6873c69a9c453b35d0ddbebd1ed
Tom <hernandeztomas584@gmail.com>
2025-06-30 00:38:35 -0300

    add: refactor

--- guias/soluciones/guia8/15.pl
+++ guias/soluciones/guia8/15.pl
@@ -3,10 +3,11 @@ desde(X, X).
 desde(X, Y) :- N is X+1, desde(N, Y). 
 
 %esTriangulo(+T)%
-esTriangulo(tri(A, B, C)) :- 
-A < B + C, BC is B-C, A > BC, 
-B < A + C, AC is A-C, B > AC,
-C < A + B, AB is A-B, C > AB.
+esTriangulo(tri(A, B, C)) :- valido(A, B, C), valido(B, C, A), valido(C, A, B).
+
+%valido(+A, +B, +C)
+valido(A, B, C) :-A < B + C.
+
 
 %perimetro(?T, ?P). Necesito usar esTriangulo(T) que recibe un tri(A, B, C). Unifico el T con el tri(A, B, C) (la variable T ahora es igual a tri(A, B, C)) para también tener los lados desestructurados. 
 %Caso -T -P: Tengo que indicar cuál es la relación entre tri(A, B, C) y un P no instanciado. La idea sería generar P infinitos. Luego, generar las combinaciones de A + B + C que arman ese P e instanciar el triangulo. Esto es generate and test porque genero todos los triangulos y luego filtro. La idea es que no se cuelgue, es decir, cuando me da uno, pueda seguir pidiendo más. Como tengo garantizado que el segundo no está instanciado puedo usar el desde común.
@@ -20,7 +21,7 @@ perimetro(T, P) :- not(ground(T)), var(P), % c.i
 
 
 %Si el perimetro está dado tengo que encontrar nros A + B + C que sumen P. Cada uno es máximo P.
-generarCombinaciones(A, B, C, P) :- between(0, P, A), between(0, P, B), between(0, P, C), P is A + B + C.
+generarCombinaciones(A, B, C, P) :- between(0, P, A), between(0, P, B), C is P-A-B, C>0.
 
 %triangulo(-T) genera todos los triangulos
 triangulo(T) :- perimetro(T, _).
