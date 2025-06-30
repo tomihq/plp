ef0261297b9006116482bf49ad3959d075def7a5
Tom <hernandeztomas584@gmail.com>
2025-06-30 13:52:03 -0300

    add: parteQueSuma

--- guias/soluciones/guia8/8.pl
+++ guias/soluciones/guia8/8.pl
@@ -1,6 +1,6 @@
 %parteQueSuma(+L, +S, -P): Es medio un problema de la mochila. Si tengo espacio agrego, caso contrario no.
 
 parteQueSuma([], 0, []).
-parteQueSuma([X | XS], S, [X | P]) :- X =< S, S2 is S-X, parteQueSuma(XS, S2, P).
-parteQueSuma([X | XS], S, P) :- X =< S, parteQueSuma(XS, S, P).
-parteQueSuma([X | XS], S, P) :- X > S, parteQueSuma(XS, S, P). 
+parteQueSuma([X | XS], S, [X | P]) :- X =< S, S2 is S-X, parteQueSuma(XS, S2, P). % Tengo espacio y lo agarro
+parteQueSuma([X | XS], S, P) :- X =< S, parteQueSuma(XS, S, P). % Tengo espacio y no lo agarro 
+parteQueSuma([X | XS], S, P) :- X > S, parteQueSuma(XS, S, P). % No me da el espacio para agarrarlo. 
